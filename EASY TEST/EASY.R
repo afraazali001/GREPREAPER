cc4bdd5d17c949ab2da645b183cfb8026ce3b1d6
Afraaz Ali <afraazali001@gmail.com>
2025-03-27 12:09:16 +0530

    Update EASY.R

--- EASY TEST/EASY.R
+++ EASY TEST/EASY.R
@@ -1,15 +1,9 @@
-# Load required packages
+# This Loads required packages
 library(ggplot2)
 library(data.table)
-
-# Load the dataset
+# dataset
 data(diamonds)
 
-# Find the row indices where "VS" appears in the clarity column
 matching_rows <- grep("VS", diamonds$clarity)
-
-# Count the number of rows that match the pattern "VS"
 num_matches <- length(matching_rows)
-
-# Print the result
 print(num_matches)
